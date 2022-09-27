de3e96059a88f5a27722712b270d50a07d1c6421
Daiki Hirose <speeed131@gmail.com>
2022-09-27 22:35:27 +0900

    feat: add shape

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import {createRoot} from 'react-dom/client';
 
+import type { ShapeProps } from "@mirohq/websdk-types";
+
+
 async function addSticky() {
   const stickyNote = await miro.board.createStickyNote({
     content: 'Hello, World!',
@@ -9,11 +12,31 @@ async function addSticky() {
   await miro.board.viewport.zoomTo(stickyNote);
 }
 
+async function addShape(shapeProps: ShapeProps) {
+  const stickyNote = await miro.board.createShape(shapeProps);
+
+  await miro.board.viewport.zoomTo(stickyNote);
+}
+
 
 function App() {
   React.useEffect(() => {
-    addSticky();
+    // addSticky();
+    addShape({
+      width: 400,
+      height: 300,
+      x: 300,
+      y: 300,
+    });
+    addShape({
+      width: 400,
+      height: 150,
+      x: 800,
+      y: 800,
+    });
   }, []);
+
+  React.useState()
 
   return (
     <div className="grid wrapper">
